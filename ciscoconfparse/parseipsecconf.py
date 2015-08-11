521407ddeaa9ea779782b00d8a1cda54f07cf8de
Patrick Herrington <pherrington@ebay.com>
2015-08-11 09:30:23 -0700

    added def main() and namespace check

--- ciscoconfparse/parseipsecconf.py
+++ ciscoconfparse/parseipsecconf.py
@@ -2,30 +2,34 @@
 
 from ciscoconfparse import CiscoConfParse
 
-cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
+def main():
 
-map_list = cisco_cfg.find_objects(r"^crypto map CRYPTO")
+    cisco_cfg = CiscoConfParse("cisco_ipsec.txt")
 
-for cryptoMap in map_list:
-    print cryptoMap.text
-    mapChildren = cryptoMap.children
-    for child in mapChildren:
-        print child.text
+    map_list = cisco_cfg.find_objects(r"^crypto map CRYPTO")
 
-print '\nCrypto maps using PFS group 2:\n'
-pfs2_list = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
-for cryptoMap in pfs2_list:
-    print cryptoMap.text
+    for cryptoMap in map_list:
+        print cryptoMap.text
+        mapChildren = cryptoMap.children
+        for child in mapChildren:
+            print child.text
 
-print '\nCrypto maps not using AES:\n'
-noaes_list = cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
-for cryptoMap in noaes_list:
-    print cryptoMap.text
-    mapChildren = cryptoMap.children
-    transformSetLine = mapChildren[1]
-    (head,transformSet) = transformSetLine.text.split('set transform-set') 
-    print transformSet
+    print '\nCrypto maps using PFS group 2:\n'
+    pfs2_list = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
+    for cryptoMap in pfs2_list:
+        print cryptoMap.text
 
+    print '\nCrypto maps not using AES:\n'
+    noaes_list = cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES-SHA")
+    for cryptoMap in noaes_list:
+        print cryptoMap.text
+        mapChildren = cryptoMap.children
+        transformSetLine = mapChildren[1]
+        (head,transformSet) = transformSetLine.text.split('set transform-set') 
+        print transformSet
+
+if __name__ == "__main__":
+    main()
 
 
 
--- yaml-json/readlist.py
+++ yaml-json/readlist.py
@@ -4,21 +4,26 @@ import yaml
 import json
 from pprint import pprint as pp
 
-with open("devicelist.yml", "r") as f:
-    my_yaml_list = yaml.load(f)
-f.close()
+def main():
 
-with open("devicelist.json", "r") as f:
-    my_json_list = json.load(f)
-f.close()
+    with open("devicelist.yml", "r") as f:
+        my_yaml_list = yaml.load(f)
+    f.close()
 
-print 'YAML list\n'
-pp(my_yaml_list)
-print '\n'
+    with open("devicelist.json", "r") as f:
+        my_json_list = json.load(f)
+    f.close()
 
-print 'JSON list\n'
-pp(my_json_list)
-print '\n'
+    print 'YAML list\n'
+    pp(my_yaml_list)
+    print '\n'
+
+    print 'JSON list\n'
+    pp(my_json_list)
+    print '\n'
+
+if __name__ == "__main__":
+    main()
 
 
 
